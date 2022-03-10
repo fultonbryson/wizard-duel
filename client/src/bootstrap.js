ea932a59cab808cbb544b0a35b32fa075bcaeec0
fultonbryson <dev.brysonfulton@gmail.com>
2022-03-10 10:15:06 -0700

    initialized application, first commit

---
+++ client/src/bootstrap.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app";
+import reducers from "./reducers";
+
+import { store } from "./store";
+
+import "./style/main.scss";
+
+function main() {
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    document.querySelector(".app-wrapper")
+  );
+}
+
+document.addEventListener("DOMContentLoaded", main);
