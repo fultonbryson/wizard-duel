1f96dbf62ed41cef3a87e02d094659651cd1697d
fultonbryson <dev.brysonfulton@gmail.com>
2022-03-11 10:32:56 -0700

    added Player table and schema

--- server/app.py
+++ server/app.py
@@ -5,10 +5,36 @@ import os
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    player_name = db.Column(db.String(26), unique=False)
+    player_health_total = db.Column(db.Integer, unique=False)
+    player_match_id = db.Column(db.Integer, unique=False)
+
+    def __init__(self, player_name, player_health_total, player_match_id):
+        self.player_name = player_name
+        self.player_health_total = player_health_total
+        self.player_match_id = player_match_id
+
+class PlayerSchema(ma.Schema):
+    class Meta:
+        fields = ('player_name', 'player_health_total', "player_match_id")
+
+player_schema = PlayerSchema()
+players_schema = PlayerSchema(many=True)    
+
+
+
 
 # CREATE
 # READ
 # UPDATE
 # DELETE
+
+if __name__ == '__main__':
+    app.run(debug=True)
