5ca6115bbeeb5152c6fab56324b697adc92e1910
fultonbryson <dev.brysonfulton@gmail.com>
2022-03-12 22:07:17 -0700

    fixed babelrc but and added directory component

--- client/src/bootstrap.js
+++ client/src/bootstrap.js
@@ -7,6 +7,7 @@ import App from "./app";
 import { store } from "./store";
 
 import Start from "./features/start/Start";
+import Directory from "./features/directory/Directory";
 
 import "./style/main.scss";
 
@@ -17,6 +18,7 @@ function main() {
         <App>
           <Switch>
             <Route path='/' exact component={Start} />
+            <Route path='/directory' exact component={Directory} />
           </Switch>
         </App>
       </BrowserRouter>
---
+++ client/src/features/directory/Directory.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { PageHeader } from "../pageHelpers/pageHelpers";
+
+const Directory = () => {
+  return (
+    <div className='directory'>
+      <PageHeader className='directory__header' header='DIRECTORY' />
+    </div>
+  );
+};
+
+export default Directory;
