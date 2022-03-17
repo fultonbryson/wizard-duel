f9b8af703cf21a364576d72e27e68b7787bf7a3b
fultonbryson <dev.brysonfulton@gmail.com>
2022-03-17 01:12:04 -0600

    created host.scss. modified and styled Host.js

--- client/src/features/host/Host.js
+++ client/src/features/host/Host.js
@@ -54,63 +54,48 @@ const Host = () => {
       });
   }
 
-  //   axios
-  //     .post(`${API_URL}/match`, {
-  //       match_format: data.match_format,
-  //       match_start_health: data.match_start_health,
-  //     })
-  //     .then((response) => {
-  //       const match = { ...response.data };
-  //       dispatch(setMatchDetails(match));
-
-  //       axios.put(`${API_URL}/player-match/${player.player_id}`, {
-  //         player_match_id: match.id,
-  //       });
-
-  //       history.push(`/lobby/${match.id}`);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }
-
   return (
     <div className='host'>
       <PageHeader className='host__header' header='HOST' />
 
-      <PageTitle className='host__title' title={`${player.player_name}`} />
+      <div className='host__content content'>
+        <PageTitle
+          className='content__player-name'
+          title={`${player.player_name}`}
+        />
 
-      <Form
-        onSubmit={onSubmit}
-        render={({ handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <div className='host__form form'>
-              <div className='form__format-select'>
-                <label>Format</label>
-                <Field name='match_format' component='select'>
-                  <option></option>
-                  <option value='Standard'>Standard</option>
-                  <option value='Commander'>Commander</option>
-                </Field>
+        <Form
+          onSubmit={onSubmit}
+          render={({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <div className='host__form form'>
+                <div className='form__format-select'>
+                  <label>Format</label>
+                  <Field name='match_format' component='select'>
+                    <option></option>
+                    <option value='Standard'>Standard</option>
+                    <option value='Commander'>Commander</option>
+                  </Field>
+                </div>
+
+                <div className='form__health-select'>
+                  <label>Start Health</label>
+                  <Field name='match_start_health' component='select'>
+                    <option></option>
+                    <option value={20}>20</option>
+                    <option value={30}>30</option>
+                    <option value={40}>40</option>
+                    <option value={50}>50</option>
+                    <option value={60}>60</option>
+                  </Field>
+                </div>
+
+                <button type='submit'>Create Match</button>
               </div>
-
-              <div className='form__health-select'>
-                <label>Start Health</label>
-                <Field name='match_start_health' component='select'>
-                  <option></option>
-                  <option value={20}>20</option>
-                  <option value={30}>30</option>
-                  <option value={40}>40</option>
-                  <option value={50}>50</option>
-                  <option value={60}>60</option>
-                </Field>
-              </div>
-
-              <button type='submit'>Create Match</button>
-            </div>
-          </form>
-        )}
-      />
+            </form>
+          )}
+        />
+      </div>
 
       <PageFooter className='host__footer' />
     </div>
